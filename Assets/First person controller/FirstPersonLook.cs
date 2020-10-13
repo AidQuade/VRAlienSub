711ebad463b62b39095cb0e6058f80e30c5e6847
aidan <aiqu0691@colorado.edu>
2020-10-13 08:56:03 -0600

    This is going to be a VR experience of exploring and interacting inside a submarine doing research in an alien ocean

---
+++ Assets/First person controller/FirstPersonLook.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+    Vector2 currentMouseLook;
+    Vector2 appliedMouseDelta;
+    public float sensitivity = 1;
+    public float smoothing = 2;
+    public Camera camera;
+
+    void Reset()
+    {
+        character = GetComponentInParent<FirstPersonMovement>().transform;
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+      
+        // Get smooth mouse look.
+        Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
+        appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
+        currentMouseLook += appliedMouseDelta;
+        currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
+
+        // Rotate camera and controller.
+        transform.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
+        character.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
+        
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit)) {
+            Transform objectHit = hit.transform;
+            
+        }
+    }
+}
